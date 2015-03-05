3d1517141d882d12bf90e33ab3c43f0f189a66b3
Ageu Duarte Junior <ageu@ageu.com.br>
2015-03-05 20:34:25 -0300

    adicionado o modulo market

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,7 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'Market'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
@@ -13,7 +14,7 @@ return array(
         // Module class.
         'module_paths' => array(
             './module',
-            './vendor',
+            './vendor','./module','./module',
         ),
 
         // An array of paths from which to glob configuration files after
